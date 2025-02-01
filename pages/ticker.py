af28a733c81e325933c17a02ee18b7315fe33297
Andy Chhuon <chhuonandy@gmail.com>
2025-02-01 14:40:43 -0500

    Ticker page defaul start and end date

--- pages/ticker.py
+++ pages/ticker.py
@@ -31,9 +31,10 @@ if ticker:
     print(f"Ticker: {ticker}")
     # Date pickers for selecting timeframe
     today = datetime.today()
-    default_start_date = today - timedelta(days=160)
+    default_start_date = st.query_params["startDate"] if "startDate" in st.query_params else today - timedelta(days=160)
+    default_end_date = st.query_params["endDate"] if "endDate" in st.query_params else today
     start_date = st.date_input('Start date', value=default_start_date, max_value=today)
-    end_date = st.date_input('End date', value=today, max_value=today)
+    end_date = st.date_input('End date', value=default_end_date, max_value=today)
 
     # Fetch and display stock data
     yf_service = YahooFinance()
