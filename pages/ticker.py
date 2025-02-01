316eb33190e0200cc3283655ad917e3cfb671fc2
camilavejar <camila.vejar01@gmail.com>
2025-02-01 18:53:29 -0500

    Update news cards

--- pages/ticker.py
+++ pages/ticker.py
@@ -83,43 +83,53 @@ if ticker:
             border: 1px solid #e6e6e6;
             border-radius: 10px;
             padding: 10px;
-            margin: 10px ;
-            height: 400px;
+            margin-bottom: 15px; 
+            height: 380px; 
             overflow: hidden;
             display: flex;
             flex-direction: column;
-            justify-content: space-between;
+            background-color: #1e1e1e;
         }
         .news-box img {
             border-radius: 10px;
-            max-height: 150px;
+            height: 150px;
             object-fit: cover;
         }
         .news-box h3 {
             color: white;
-            font-size: 1.2em;
-            margin: 5px ;
+            font-size: 1.1em;
+            margin: 0;
+            line-height: 1.2; 
         }
         .news-box p {
             color: white;
-            font-size: 1em;
-            margin: 10px 0;
+            font-size: 0.9em; 
+            margin: 0; 
             overflow: hidden;
             text-overflow: ellipsis;
             display: -webkit-box;
             -webkit-line-clamp: 3;
             -webkit-box-orient: vertical;
-            padding-bottom: 5px; 
+        }
+        .stMarkdownContainer {
+            padding: 0;
+        }
+        .stHorizontalContainer {
+            display: flex;
+            flex-wrap: wrap; 
+            justify-content: space-between; 
         }
         </style>
         """,
         unsafe_allow_html=True
     )
-    # Display in 3x3 grid
+    # Display in 3 columns
     for i in range(0, len(news_data), 3):
         cols = st.columns(3)
         for col, news in zip(cols, news_data[i:i+3]):
             content = news['content']
+            # Check if there is no image 
+            thumbnail_url = content['thumbnail']['resolutions'][0]['url'] if content['thumbnail'] else None
             print('dfsdfsd')
             print(content)
             thumbnail_url = content['thumbnail']['resolutions'][0]['url']
