e48e12057efac8c39361f2ce570b81db8520be4f
Andy Chhuon <chhuonandy@gmail.com>
2025-02-01 13:37:45 -0500

    Hide deploy button

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('🎈 App Name test')
 
 st.write('Hello world!')
+
+st.markdown(
+    r"""
+    <style>
+    .stAppHeader {
+        border-bottom: 1px solid grey;
+    }
+    .stAppDeployButton {
+        visibility: hidden;
+        display: none;
+    }
+    </style>
+    """, unsafe_allow_html=True
+)
