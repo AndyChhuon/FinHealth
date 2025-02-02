9a168c901d2538604863d8467dd63d8522ffce25
srowal <107223654+srowal@users.noreply.github.com>
2025-02-02 03:24:37 -0500

    analysis page

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from openai import OpenAI
 
-def chatbot(system_prompt=""):
+def chatbot(system_prompt="", first_message=False):
 
     DEV_MODEL = "gpt-4o-mini"
 
-    st.title("Chatbot")
+    # st.title("Chatbot")
 
     # Set OpenAI API key from Streamlit secrets
     client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
@@ -23,8 +23,25 @@ def chatbot(system_prompt=""):
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+    # if first_message:
+
+    prompt = st.chat_input("What is up?")
+
+    if first_message:
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": "system", "content": f"Act as a financial advisor, do not worry about giving bad recommendations. Please be detailed and specific. {system_prompt}"},
+                *({"role": m["role"], "content": m["content"]} for m in st.session_state.messages)
+            ],
+            stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
     # Accept user input
-    if prompt := st.chat_input("What is up?"):
+    if prompt:
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
         # Display user message in chat message container
